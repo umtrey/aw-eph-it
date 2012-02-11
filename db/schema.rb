036197b502786dea155f9d6268c2f0052e0dec92
Steve Benjamins <steve@stevebenjamins.com>
2012-02-11 15:38:17 -0600

    More crawlers

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+
 # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
@@ -10,6 +11,7 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended to check this file into your version control system.
+
 
 ActiveRecord::Schema.define(:version => 20120211193700) do
 
@@ -50,6 +52,7 @@ ActiveRecord::Schema.define(:version => 20120211193700) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
 
   create_table "rules", :force => true do |t|
     t.integer  "ingredient_id"
