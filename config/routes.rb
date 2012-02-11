0ea1c9ba99b7303787e9a49aff517da22b4c2395
Trey Springer <dfsiii@gmail.com>
2012-02-11 14:28:04 -0600

    fixed routes after my stupid error

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,10 @@
 AwEphIt::Application.routes.draw do
 
-  get 'game_search' => "game_search#index"
   get '/index' => "game_search#index"
   get '/list' => "game_search#list"
-<<<<<<< HEAD
-
   get '/game_search' => "game_search#index"
-
-=======
   
   resources :rules
->>>>>>> trey2
   resources :sources
   resources :gameingredients
   resources :ingredients do
