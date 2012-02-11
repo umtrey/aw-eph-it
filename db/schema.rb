3a26d510cada4ec2e6e49aaf004821a58bad293e
shockt88@gmail.com <shockt88>
2012-02-11 17:55:19 -0600

    Updated gitignore to display crawler data

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-
 # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
@@ -11,7 +10,6 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended to check this file into your version control system.
-
 
 ActiveRecord::Schema.define(:version => 20120211193700) do
 
@@ -52,7 +50,6 @@ ActiveRecord::Schema.define(:version => 20120211193700) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
 
   create_table "rules", :force => true do |t|
     t.integer  "ingredient_id"
