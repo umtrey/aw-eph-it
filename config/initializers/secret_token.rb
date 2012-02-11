60be8c3169731a9bbb93f5f34b9f8fb577d7f394
Trey Springer <dfsiii@gmail.com>
2012-02-10 21:02:28 -0600

    real first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AwEphIt::Application.config.secret_token = '62b4335988e6f89e6e17fb912920ce8dbc1366df0d0052de4126179c649b5d84c6983b777444dde870cfaf76985aaae948d032d65e7d823979c4110adf1fb999'
